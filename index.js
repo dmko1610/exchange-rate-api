5f66808c4bd79869023e12e0050faed6c4b4ab9f
dmko1610 <trigger1610@gmail.com>
2023-01-07 16:53:52 +0500

    Add two endpoints
    Main endpoint gets an abilitiy to get current exchange rate for specific pair of currency

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+const PORT = process.env.PORT || 8000
+const express = require("express")
+const axios = require("axios")
+const cheerio = require("cheerio")
 
+const app = express()
+
+const rate = {}
+
+app.get("/", (req, res) => {
+  res.json("Welcome to exchange rate api!")
+})
+
+app.get(`/currencies/:currencyCode`, (req, res) => {
+  axios.get(`https://www.investing.com/currencies/${req.params.currencyCode}`)
+    .then((response) => {
+      const html = response.data
+      const $ = cheerio.load(html)
+      $('[data-test="instrument-price-last"]', html)
+        .map(function () {
+          const rateValue = $(this).text()
+          rate.value = rateValue
+        })
+      res.json(rate)
+    }).catch((err) => console.error(err))
+})
+
+app.listen(PORT, () => console.log(`Server is starting on port ${PORT}`))
